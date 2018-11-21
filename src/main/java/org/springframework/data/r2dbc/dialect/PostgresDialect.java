32deac9127ac7dfc4acf4ceb629685613b042adb
Mark Paluch <mpaluch@pivotal.io>
2018-11-21 14:11:37 +0100

    #20 - Introduce BindableOperation to represent query operations.
    BindableOperation encapsulates statements/queries that can accept parameters. Use BindableOperation for statements through DatabaseClient.
    Extract SQL creation.
    
    Split integration test into abstract base class that can be implemented with a database-specific test class.

--- src/main/java/org/springframework/data/r2dbc/dialect/PostgresDialect.java
+++ src/main/java/org/springframework/data/r2dbc/dialect/PostgresDialect.java
@@ -14,13 +14,60 @@ public class PostgresDialect implements Dialect {
 
 	private static final BindMarkersFactory INDEXED = BindMarkersFactory.indexed("$", 1);
 
+	private static final LimitClause LIMIT_CLAUSE = new LimitClause() {
+
+		/* 
+		 * (non-Javadoc)
+		 * @see org.springframework.data.r2dbc.dialect.LimitClause#getClause(long, long)
+		 */
+		@Override
+		public String getClause(long limit, long offset) {
+			return String.format("LIMIT %d OFFSET %d", limit, offset);
+		}
+
+		/* 
+		 * (non-Javadoc)
+		 * @see org.springframework.data.r2dbc.dialect.LimitClause#getClause(long)
+		 */
+		@Override
+		public String getClause(long limit) {
+			return "LIMIT " + limit;
+		}
+
+		/* 
+		 * (non-Javadoc)
+		 * @see org.springframework.data.r2dbc.dialect.LimitClause#getClausePosition()
+		 */
+		@Override
+		public Position getClausePosition() {
+			return Position.END;
+		}
+	};
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.springframework.data.r2dbc.dialect.Dialect#getBindMarkersFactory()
+	 */
 	@Override
 	public BindMarkersFactory getBindMarkersFactory() {
 		return INDEXED;
 	}
 
+	/* 
+	 * (non-Javadoc)
+	 * @see org.springframework.data.r2dbc.dialect.Dialect#returnGeneratedKeys()
+	 */
 	@Override
 	public String returnGeneratedKeys() {
 		return "RETURNING *";
+	}
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.springframework.data.r2dbc.dialect.Dialect#limit()
+	 */
+	@Override
+	public LimitClause limit() {
+		return LIMIT_CLAUSE;
 	}
 }
